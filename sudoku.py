5a8ee7630d37c6f6bb7effafd015b962c4c9f9b9
zainab_majid <zainabmajid2408@gmail.com>
2024-05-26 12:55:22 -0600

    created functions

---
+++ sudoku.py
@@ -0,0 +1,9 @@
+#finds the next row and col on the puzzle that is empty
+def findNextEmpty(puzzle):
+    #define any open spaces with -1
+    
+def solveSudoku(puzzle):
+    #mutates puzzle to be the solution if a solution exists
+
+    #1. choose where to make the first guess
+    row, col = findNextEmpty(puzzle)
