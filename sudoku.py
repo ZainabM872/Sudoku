2ce588bdc6be8208bcac503d339612cb23ef4a62
zainab_majid <zainabmajid2408@gmail.com>
2024-05-26 12:57:39 -0600

    Done step 1:  where to make the first guess

--- sudoku.py
+++ sudoku.py
@@ -2,8 +2,16 @@
 def findNextEmpty(puzzle):
     #define any open spaces with -1
     
+    for r in range(9):
+        for c in range(9):
+            if puzzle[r][c] == -1:
+                return r,c
+    return None, None    #return row, col tuple or (None, None) if the board is full: no empty spaces
+
 def solveSudoku(puzzle):
     #mutates puzzle to be the solution if a solution exists
 
     #1. choose where to make the first guess
     row, col = findNextEmpty(puzzle)
+    if row is None:
+        return True
